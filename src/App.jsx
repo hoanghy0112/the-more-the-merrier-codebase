f126b8a8c6ce8afe65fe6e9988261a0dc055676f
hoanghy <hoanghy01122003@gmail.com>
2022-10-08 01:24:47 +0700

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import "./App.scss";
+import styles from './App.module.scss';
+
+function App() {
+  return <div className={styles.App}>Source code</div>;
+}
+
+export default App;
